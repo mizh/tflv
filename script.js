39a4c58ecf2e81c371c252e3b64b87a3969042fe
mizh <mparente@ualberta.ca>
2017-04-02 14:31:29 -0600

    trying to see the difference between one version of this script that works and one that does not

--- script.js
+++ script.js
@@ -1,35 +1,37 @@
 
-var width = 420,
-    barHeight = 20;
+var width = 960,
+    barHeight = 500;
 
-var x = d3.scaleLinear()
-    .range([0, width]);
+var y = d3.scaleLinear()
+    .range([height, 0]);
 
 var chart = d3.select(".chart")
-    .attr("width", width);
+	.attr("width", width)
+	.attr("height", height);
 
 d3.csv("tweets.csv", type, function(error, data) {
-  x.domain([0, d3.max(data, function(d) { return d.tweet_id; })]);
+	y.domain([0, d3.max(data, function(d) { return d.tweet_id; })]);
+  
+	var barWidth = width / data.length;
 
-  chart.attr("height", barHeight * data.length);
+	var bar = chart.selectAll("g")
+		.data(data)
+	.enter().append("g")
+		.attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; });
 
-  var bar = chart.selectAll("g")
-      .data(data)
-    .enter().append("g")
-      .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+	bar.append("rect")
+		.attr("y", function(d) { return y(d.tweet_id); })
+		.attr("height", function(d) { return height - y(d.tweet_id); })
+		.attr("width", barWidth - 1);
 
-  bar.append("rect")
-      .attr("width", function(d) { return x(d.tweet_id); })
-      .attr("height", barHeight - 1);
-
-  bar.append("text")
-      .attr("x", function(d) { return x(d.tweet_id) - 3; })
-      .attr("y", barHeight / 2)
-      .attr("dy", ".35em")
-      .text(function(d) { return d.tweet_id; });
+	bar.append("text")
+		.attr("x", barWidth / 2)
+		.attr("y", function(d) { return y(d.tweet_id) + 3; })
+		.attr("dy", ".75em")
+		.text(function(d) { return d.tweet_id; });
 });
 
 function type(d) {
-  d.tweet_id = +d.tweet_id; // coerce to number
-  return d;
+	d.tweet_id = +d.tweet_id; 
+	return d;
 }
