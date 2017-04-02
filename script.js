81e9e3fa4fcef1281bb07cc7dc483c2433dca0c8
mizh <mparente@ualberta.ca>
2017-04-02 03:04:10 -0600

    arbitrary updates based on random knowledge acquisition

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
-var w=960,h=500,
-svg=d3.select("#chart")
-.append("svg")
-.attr("width",w)
-.attr("height",h);
- 
-var text=svg
-.append("text")
-.text("hello world")
-.attr("y",50);
+
+var width = 420,
+    barHeight = 20;
+
+var x = d3.scaleLinear()
+    .range([0, width]);
+
+var chart = d3.select(".chart")
+    .attr("width", width);
+
+d3.csv("tweets.csv", type, function(error, data) {
+  x.domain([0, d3.max(data, function(d) { return d.tweet_id; })]);
+
+  chart.attr("height", barHeight * data.length);
+
+  var bar = chart.selectAll("g")
+      .data(data)
+    .enter().append("g")
+      .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+  bar.append("rect")
+      .attr("width", function(d) { return x(d.tweet_id); })
+      .attr("height", barHeight - 1);
+
+  bar.append("text")
+      .attr("x", function(d) { return x(d.tweet_id) - 3; })
+      .attr("y", barHeight / 2)
+      .attr("dy", ".35em")
+      .text(function(d) { return d.tweet_id; });
+});
+
+function type(d) {
+  d.tweet_id = +d.tweet_id; // coerce to number
+  return d;
+}
