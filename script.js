a9708d05673a9f9ac7080af0547bdbbdda56c7d0
mizh <mparente@ualberta.ca>
2017-04-03 18:40:05 -0600

    creates a labelled  rectangle for every unique day in the dataset

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 var margin = {right: 300};
 
 var width = 960 - margin.right,
-    barHeight = 20;
+	barWidth = 600;
+    barHeight = 100;
 
 var x = d3.scaleLinear()
     .range([0, width]);
 
 var chart = d3.select(".chart")
     .attr("width", width + margin.right);
+    
+var currDay = +"00000000";
 
 d3.csv("tweets.csv", row, function(error, data) {
   x.domain([0, d3.max(data, function(d) { return d.tweet_id; })]);
@@ -17,27 +20,39 @@ d3.csv("tweets.csv", row, function(error, data) {
   var bar = chart.selectAll("g")
       .data(data)
     .enter().append("g")
+      .filter(function(d) { return newDay(d.day); })
       .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
-
+  
   bar.append("rect")
-      .attr("width", function(d) { return x(d.tweet_id); })
+  	  .filter(function(d) { return newDay(d.day); })
+      .attr("width", barWidth)
       .attr("height", barHeight - 1);
-
+      
   bar.append("text")
-      .attr("x", function(d) { return x(d.tweet_id); })
-      .style("text-anchor", "end")
+  	  .filter(function(d) { return newDay(d.day); })
+      .attr("x", barWidth + 10)
+      .style("text-anchor", "start")
       .attr("y", barHeight / 2)
       .attr("dy", ".35em")
-      .text(function(d) { return d.day; });
+      .text(function(d) { return d.prettyDay; });
 });
 
 var parseDate = d3.timeParse("%Y-%m-%d %H:%M:%S %Z"),
-	formatDay = d3.timeFormat("%m-%d-%Y"),
+	formatDay = d3.timeFormat("%m%d%Y"),
+	formatPrettyDay = d3.timeFormat("%A %B %e, %Y"),
 	formatYear = d3.timeFormat("%Y");
 
 function row(d) {
   return {
-  day: formatDay(parseDate(d.timestamp)),
+  day: +formatDay(parseDate(d.timestamp)),
+  prettyDay: formatPrettyDay(parseDate(d.timestamp)),
   tweet_id: +d.tweet_id 
 };
+}
+
+function newDay(day) {
+  if (day != currDay) {
+    currDay = day;
+    return true }
+  else { return false };
 }
