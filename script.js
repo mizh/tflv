0b4cf5f8f8457b78fb7af981f076b10e15cc47e6
mizh <mparente@ualberta.ca>
2017-04-04 15:56:07 -0600

    made the dots get big and red upon mouseover + added tooltips + made the source code indentation prettier

--- script.js
+++ script.js
@@ -9,41 +9,48 @@ var t = d3.scaleTime()
 	.range([0, barWidth]);
 
 var chart = d3.select(".chart")
-    .attr("width", width + margin.right);
+	.attr("width", width + margin.right);
     
 var currDay = +"00000000";
 dayI = -1;
 
 d3.csv("tweets.csv", row, function(error, data) {
 
-  chart.attr("height", barHeight * data.length);
+	chart.attr("height", barHeight * data.length);
 
-  var bar = chart.selectAll("g")
-      .data(data)
-    .enter().append("g")
-      .filter(function(d) { return newDay(d.day); })
-      .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+	var bar = chart.selectAll("g")
+		.data(data)
+	.enter().append("g")
+		.filter(function(d) { return newDay(d.day); })
+		.attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
   
-  bar.append("rect")
-      .attr("width", barWidth)
-      .attr("height", barHeight - 1);
+	bar.append("rect")
+		.attr("width", barWidth)
+		.attr("height", barHeight - 1);
       
-  bar.append("text")
-      .attr("x", barWidth + 10)
-      .style("text-anchor", "start")
-      .attr("y", barHeight / 2)
-      .attr("dy", ".35em")
-      .text(function(d) { return d.prettyDay; });
+	bar.append("text")
+		.attr("x", barWidth + 10)
+		.style("text-anchor", "start")
+		.attr("y", barHeight / 2)
+		.attr("dy", ".35em")
+		.text(function(d) { return d.prettyDay; });
       
-  var dots = chart.selectAll("circle")
-  	  .data(data)
-  	.enter().append("circle")
-  	  //.filter(function(d) { return dayIndex(d.day); })
-      .attr("transform", function(d) { return "translate(0," + newDayIndex(d.day) * barHeight + ")"; })
-  	  .attr("cx", function (d) { return t(d.moment); })
-      .attr("cy", barHeight / 2)
-  	  .attr("r", 5);
-  
+	var dots = chart.selectAll("circle")
+		.data(data)
+	.enter().append("circle")
+		.attr("transform", function(d) { return "translate(0," + newDayIndex(d.day) * barHeight + ")"; })
+		.attr("cx", function(d) { return t(d.moment); })
+		.attr("cy", barHeight / 2)
+		.attr("r", 5)
+		.on("mouseover", function(d) {
+			d3.select(this).attr("r", 10).style("fill", "red");
+		})                  
+		.on("mouseout", function(d) {
+			d3.select(this).attr("r", 5).style("fill", "black");
+		})
+		.append("svg:title")
+		.text(function(d) { return d.text });
+  	  
 });
 
 var parseDate = d3.timeParse("%Y-%m-%d %H:%M:%S %Z"),
@@ -55,25 +62,34 @@ var parseDate = d3.timeParse("%Y-%m-%d %H:%M:%S %Z"),
 	formatYear = d3.timeFormat("%Y");
 
 function row(d) {
-  return {
-  moment: new Date(1999,12,31,+hour(parseDate(d.timestamp)),+minute(parseDate(d.timestamp)),+second(parseDate(d.timestamp))),
-  day: +formatDay(parseDate(d.timestamp)),
-  prettyDay: formatPrettyDay(parseDate(d.timestamp)),
-  tweet_id: +d.tweet_id 
-};
+	return {
+		moment: new Date(1999,12,31,+hour(parseDate(d.timestamp)),+minute(parseDate(d.timestamp)),+second(parseDate(d.timestamp))),
+		day: +formatDay(parseDate(d.timestamp)),
+		prettyDay: formatPrettyDay(parseDate(d.timestamp)),
+		tweet_id: +d.tweet_id,
+		text: cleanText(d.text)
+		};
 }
 
 function newDay(day) {
-  if (day != currDay) {
-    currDay = day;
-    return true }
-  else { return false };
+	if (day != currDay) {
+		currDay = day;
+		return true }
+	else { return false };
 }
 
 function newDayIndex(day) {
-  if (day != currDay) {
-    currDay = day;
-    dayI = dayI + 1;
-    return dayI }
-  else { return dayI };
+	if (day != currDay) {
+		currDay = day;
+		dayI = dayI + 1;
+		return dayI }
+	else { return dayI };
+}
+
+function cleanText(text) {
+	var find = 'http(.*)';
+	var re = new RegExp(find, 'g');
+	text = text.replace(re, '');
+	text = text.replace(/&amp;/g, '&');
+	return text
 }
