76f8961d7680c0e7744f3832937a7997eac5cbc7
mizh <mparente@ualberta.ca>
2017-04-07 20:04:25 -0600

    improved style, added a cleaned up version of tweets.csv, turned coffee nodes red

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-var margin = {top:50, bottom:50, left:600, right: 600};
+var margin = {top:50, bottom:50, left:320, right: 200};
 
 var width = 2000 - margin.right - margin.left,
-	barWidth = 600;
-	barHeight = 100;
+	barWidth = 960;
+	barHeight = 100,
+	dotRadius = 4.6;
+	dotOffset = 5;
+
+var chart = d3.select(".chart")
+	.attr("width", width + margin.right + margin.left);
 
 var t = d3.scaleTime()
 	.domain([new Date(1999,12,31,0,0,0), new Date(1999,12,31,23,59,59)])
 	.range([0, barWidth]);
 
-var chart = d3.select(".chart")
-	.attr("width", width + margin.right + margin.left);
+var axis = d3.axisTop(t)
+	.tickFormat(d3.timeFormat("%H:%M"));
 
 var tip = d3.tip()
 	.attr("class", "d3-tip")
@@ -20,38 +25,49 @@ chart.call(tip);
 var currDay = +"00000000",
 dayI = -1;
 
-d3.csv("tweets.csv", row, function(error, data) {
-	var bar = chart.selectAll("g")
+d3.csv("tweets_clean.csv", row, function(error, data) {
+		
+	chart.append("g")
+		.attr("class", "axis")
+		.call(axis);
+		
+	var bars = chart.selectAll("bar")
 		.data(data)
 	.enter().append("g")
 		.filter(function(d) { return newDay(d.day); })
 		.attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
-  
-	bar.append("rect")
-		.attr("width", barWidth)
-		.attr("height", barHeight - 1);
+	
+	bars.append("line")
+		.attr("x1", 0)
+		.attr("y1", function(d, i) { return barHeight/2; })
+		.attr("x2", barWidth)
+		.attr("y2", function(d, i) { return barHeight/2; });
       
-	bar.append("text")
-		.attr("x", barWidth + 10)
-		.style("text-anchor", "start")
+	bars.append("text")
+		.attr("x", -60)
+		.style("text-anchor", "end")
 		.attr("y", barHeight / 2)
 		.attr("dy", ".35em")
-		.text(function(d) { return wrap(d.prettyDay.toString(), 25, 'X', false); });
-      
-	var dots = chart.selectAll("circle")
+		.text(function(d) { return d.prettyDay; });
+		      
+	var dots = chart.selectAll("dot")
 		.data(data)
-	.enter().append("circle")
+	.enter().append("g");
+	
+	dots.append("circle")
 		.attr("transform", function(d) { return "translate(0," + newDayIndex(d.day) * barHeight + ")"; })
 		.attr("cx", function(d) { return t(d.moment); })
-		.attr("cy", barHeight / 2)
-		.attr("r", 5)
+		.attr("cy", function(d, i) { if (i%2 == 0) { return barHeight/2 + dotOffset } else { return barHeight/2 - dotOffset }; })
+		.attr("r", dotRadius)
+		.style("fill", function(d) { if (d.text.includes("coffee")) { return "red" } else { return "black" }; })
 		.on("mouseover", function(d) {
-			tip.show(wrap(d.text, 30, "<br>", false));
-			d3.select(this).attr("r", 10).style("fill", "red");
+			tip.show(wrap(d.text, 40, "<br>", false));
+			d3.select(this).attr("r", dotRadius * 2).style("fill", "#87CEEB");
 		})                  
 		.on("mouseout", function(d) {
 			tip.hide(d.text);
-			d3.select(this).attr("r", 5).style("fill", "black");
+			d3.select(this).attr("r", dotRadius)
+			.style("fill", function(d) { if (d.text.includes("coffee")) { return "red" } else { return "black" }; })
 		});
 		
 		chart.attr("height", barHeight * (dayI+1) + margin.top + margin.bottom) 
